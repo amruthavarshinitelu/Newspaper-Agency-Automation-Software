ffb168c43e243403a40d153bc3870a15d57665ee
Amruthavarshini_telu <120393408+amruthavarshinitelu@users.noreply.github.com>
2023-05-31 14:36:55 +0530

    Revert "ready to host"

--- naas/settings.py
+++ naas/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*864fm*gc6ox05+aqjpis6hv($t$xl^i@=fmd+)@&w^^*b&%mf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
